1aaac4db0a61eb950e932d3dca916e7c2df3dac5
Lester Roxas <roxas.lester@auf.edu.ph>
2021-05-26 21:02:47 +0800

    Add project files.

---
+++ PDC03_MOD6/ViewModel/EmployeeViewModel.cs
@@ -0,0 +1,50 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using PDC03_MOD6.Model;
+
+namespace PDC03_MOD6.ViewModel
+{
+   public class EmployeeViewModel
+    {
+        private Services.DatabaseContext getContext()
+        {
+            return new Services.DatabaseContext();
+        }
+
+        public async Task<List<EmployeeModel>> GetAllEmployees()
+        {
+            var _dbContext = getContext();
+            var res = await _dbContext.Employees.ToListAsync();
+            return res;
+
+        }
+        public async Task<int> UpdateEmployee(EmployeeModel obj)
+        {
+            var _dbContext = getContext();
+            _dbContext.Employees.Update(obj);
+            int c = await _dbContext.SaveChangesAsync();
+            return c;
+        }
+
+        public int InsertEmployee(EmployeeModel obj)
+        {
+            var _dbContext = getContext();
+            _dbContext.Employees.Add(obj);
+            int c = _dbContext.SaveChanges();
+            return c;
+        }
+
+        public int DeleteEmployee(EmployeeModel obj)
+        {
+            var _dbContext = getContext();
+            _dbContext.Employees.Remove(obj);
+            int c = _dbContext.SaveChanges();
+            return c;
+        }
+    }
+}
